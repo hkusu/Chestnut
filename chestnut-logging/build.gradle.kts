50dc4befaa4526f67856885234aa7926f9c03752
Hiroyuki Kusu <kusu0806@gmail.com>
2024-10-10 21:09:21 +0900

    Add logging module

---
+++ chestnut-logging/build.gradle.kts
@@ -0,0 +1,50 @@
+// import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
+// import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.androidLibrary)
+    id("module.publication")
+}
+
+kotlin {
+    // jvm()
+    androidTarget {
+        publishLibraryVariants("release")
+        // @OptIn(ExperimentalKotlinGradlePluginApi::class)
+        // compilerOptions {
+        //     jvmTarget.set(JvmTarget.JVM_1_8)
+        // }
+        compilations.all {
+            kotlinOptions {
+                jvmTarget = "1.8"
+            }
+        }
+    }
+    iosX64()
+    iosArm64()
+    iosSimulatorArm64()
+    // linuxX64()
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                //put your multiplatform dependencies here
+                implementation(project(":chestnut-core"))
+            }
+        }
+        val commonTest by getting {
+            dependencies {
+                implementation(libs.kotlin.test)
+            }
+        }
+    }
+}
+
+android {
+    namespace = "io.github.hkusu.chestnut.logging"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+    defaultConfig {
+        minSdk = libs.versions.android.minSdk.get().toInt()
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,3 +16,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "Chestnut"
 include(":chestnut-core")
+include(":chestnut-logging")
